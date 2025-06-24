ab25cf54efd952822f78a84aeb286945809f95dc
Anna Dvořáková <98867919@cuni.cz>
2025-06-24 16:01:51 +0200

    add repository to be anonymously shared

---
+++ PyCantus/pycantus/filtration/filter.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+"""
+This module defines the base class for filters used in the PyCantus library.
+It provides a structure for creating and applying filters to chant data.
+"""
+import yaml
+import os
+
+from collections import defaultdict
+from pycantus.models.source import EXPORT_SOURCES_FIELDS
+from pycantus.models.chant import EXPORT_CHANTS_FIELDS
+
+__version__ = "0.0.4"
+__author__ = "Anna Dvorakova"
+
+
+
+class Filter:
+    """
+    Base class for filters over Corpus data lists:
+    - chants - objects of class pycantus.models.Chant
+    - sources - objects of class pycantus.models.Source
+    - melodies - objects of class pycantus.models.Melody
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+        self.filters_include = defaultdict(list)
+        self.filters_exclude = defaultdict(list)
+    
+    def add_value_include(self, field : str, values : list[str]):
+        """
+        Add a value to the filter for a specific field to be included.
+        """
+        if not isinstance(values, list):
+            raise TypeError(f"Argument 'values' must be of type list.")
+        
+        if field not in EXPORT_CHANTS_FIELDS and field not in EXPORT_SOURCES_FIELDS:
+            raise ValueError(f"Field '{field}' is not a valid chant or source field.")
+
+        self.filters_include[field] += values
+        self.filters_include[field] = list(set(self.filters_include[field])) # discard dupliates
+            
+    def add_value_exclude(self, field : str, values : list[str]):
+        """
+        Add a value to the filter for a specific field to be excluded.
+        """
+        if not isinstance(values, list):
+            raise TypeError(f"Argument 'values' must be of type list.")
+        
+        if field not in EXPORT_CHANTS_FIELDS and field not in EXPORT_SOURCES_FIELDS:
+            raise ValueError(f"Field '{field}' is not a valid chant or source field.")
+
+        self.filters_exclude[field] += values
+        self.filters_exclude[field] = list(set(self.filters_exclude[field])) # discard dupliates
+
+    
+    def apply(self, chants : list, sources : list, melodies : list):
+        """
+        Apply the filter to the given data.
+        Returns filtered chants and sources based on the filter criteria.
+        If no values for field are specified we expect that user do not care about the field.
+        If no filter is to be applied, returns the original lists.
+        """
+        filter_fields = set(self.filters_include.keys()).union(self.filters_exclude.keys())
+        if len(filter_fields) == 0:
+            print("No filtering applied because no filtration values were present, returning original lists.")
+            return chants, sources, melodies
+
+        filtered_sources= []
+        discarded_srclinks = set()
+        source_filter_fields = set(EXPORT_SOURCES_FIELDS).intersection(filter_fields)
+        if source_filter_fields:
+            for source in sources:
+                include_pass = all(
+                    field not in self.filters_include or not self.filters_include[field] or
+                    getattr(source, field, None) in self.filters_include[field]
+                    for field in source_filter_fields
+                )
+                exclude_pass = all(
+                    field not in self.filters_exclude or not self.filters_exclude[field] or
+                    getattr(source, field, None) not in self.filters_exclude[field]
+                    for field in source_filter_fields
+                )
+
+                if include_pass and exclude_pass:
+                    filtered_sources.append(source)
+                else:
+                    discarded_srclinks.add(source.srclink)
+
+            sources = filtered_sources    
+            
+
+        filtered_chants = []
+        chants_filter_fields = set(EXPORT_CHANTS_FIELDS).intersection(filter_fields)
+        if chants_filter_fields or discarded_srclinks:
+            for chant in chants:
+                if chant.srclink in discarded_srclinks:
+                    continue
+
+                include_pass = all(
+                    field not in self.filters_include or not self.filters_include[field] or
+                    getattr(chant, field, None) in self.filters_include[field]
+                    for field in chants_filter_fields
+                )
+                exclude_pass = all(
+                    field not in self.filters_exclude or not self.filters_exclude[field] or
+                    getattr(chant, field, None) not in self.filters_exclude[field]
+                    for field in chants_filter_fields
+                )
+
+                if include_pass and exclude_pass:
+                    filtered_chants.append(chant)
+
+            chants = filtered_chants
+             
+        if len(melodies) > 0:
+            filtered_melodic_chantlinks = {chant.chantlink for chant in chants if chant._has_melody}
+            # Discard melodies that are not in the filtered chants
+            filtered_melodies = []
+            for melody in melodies:
+                if melody is not None and melody.chantlink in filtered_melodic_chantlinks:
+                    filtered_melodies.append(melody)
+            melodies = filtered_melodies
+
+        return chants, sources, melodies
+
+    def delete_field(self, field):
+        """
+        Delete a field from both the include and the exclude filters.
+        """
+        if field in self.filters_exclude.keys():
+            del self.filters_exclude[field]
+        elif field in self.filters_include.keys():
+            del self.filters_include[field]
+
+    def __str__(self):
+        """
+        Returns yaml style string representation of filter
+        configuration.
+        """
+        setting = {
+            'name': self.name,
+            'include_values' : dict(self.filters_include),
+            'exclude_values' : dict(self.filters_exclude)
+        }
+        return yaml.dump(setting, allow_unicode=True, sort_keys=False)
+    
+    def export_yaml(self, path : str):
+        """
+        Export the filter configuration to a directory given as path 
+        as '{self.name}.yaml' yaml file.
+        """
+        setting = {
+            'name': self.name,
+            'include_values' : dict(self.filters_include),
+            'exclude_values' : dict(self.filters_exclude)
+        }
+        file_name = f"{self.name}.yaml"
+        file_path = os.path.join(path, file_name)
+
+        try:
+            os.makedirs(path, exist_ok=True)  # Create directory if it doesn't exist
+            with open(file_path, 'w', encoding='utf-8') as f:
+                yaml.dump(setting, f, allow_unicode=True, sort_keys=False)
+            print(f"Filter '{self.name}' successfully exported to: {file_path}")
+        except Exception as e:
+            print(f"Error exporting filter to YAML: {e}")
+    
+    def import_yaml(self, config_file_path : str):
+        """
+        Function that reads YAML file and transforms it into 
+        self.filter_include and self.filter_exclude dictionaries.
+        """
+        try:
+            with open(config_file_path, 'r') as f:
+                yaml_content = yaml.safe_load(f)
+            self.name = yaml_content['name']
+            self.filters_include = defaultdict(list, yaml_content['include_values'])
+            self.filters_exclude = defaultdict(list, yaml_content['exclude_values'])
+        except:
+            raise IOError(f"Filter configuration file on path {config_file_path} load failed!")
---
+++ PyCantus/pycantus/models/corpus.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+"""
+This module contains the Corpus class, which represents a collection of chants and sources.
+It provides methods for loading, filtering, and exporting data related to the chants and sources.
+"""
+
+import pandas as pd
+
+from pycantus.models.chant import Chant
+from pycantus.models.source import Source
+from pycantus.dataloaders.loader import CsvLoader
+from pycantus.filtration.filter import Filter
+
+
+__version__ = "0.0.4"
+__author__ = "Anna Dvorakova"
+
+
+class Corpus():
+    """
+    pycantus Corpus class 
+        - represents a collection of chants and sources (piece of repertoire)
+        - provides methods for loading, filtering, and exporting data related to the chants and sources
+        - can be editable or not (if not, it is locked for editing)
+    """
+    def __init__(self,
+                 chants_filepath,
+                 sources_filepath=None,
+                 chants_fallback_url=None,
+                 sources_fallback_url=None,
+                 other_parameters=None,
+                 is_editable=False,
+                 **kwargs):
+        
+        self.chants_filepath = chants_filepath
+        self.sources_filepath = sources_filepath
+        self.chants_fallback_url = chants_fallback_url
+        self.sources_fallback_url = sources_fallback_url
+        self.other_download_parameters = other_parameters
+        self.is_editable = is_editable
+
+        loader = CsvLoader(self.chants_filepath, self.sources_filepath, 
+                           self.chants_fallback_url, self.sources_fallback_url, 
+                           other_parameters)
+        chants, sources = loader.load()
+
+        self._chants = chants
+        self._sources = sources
+        self._melodies = []
+
+        if not self.is_editable:
+            self._lock_chants()
+            self._lock_sources()
+
+    
+    def _lock_chants(self):
+        """ Sets all chants to locked. """
+        for c in self._chants:
+            c.locked = True
+
+    def _lock_sources(self):
+        """ Sets all sources to locked. """
+        for s in self._sources:
+            s.locked = True
+    
+    def _lock_melodies(self):
+        """ Sets all sources to locked. """
+        for m in self._melodies:
+            m.locked = True
+
+    @property #getter
+    def chants(self):
+        return self._chants
+    
+    @chants.setter
+    def chants(self, new_chants: list[Chant]):
+        if self.is_editable:
+            self._chants = new_chants
+        else:
+            raise PermissionError('Corpus is not editable, cannot replace chant list.')
+
+    @property #getter
+    def sources(self):
+        return self._sources
+    
+    @sources.setter
+    def sources(self, new_sources: list[Source]):
+        if self.is_editable:
+            self._sources = new_sources
+        else:
+            raise PermissionError('Corpus is not editable, cannot replace sources list.')
+    
+    @property #getter
+    def melodies(self):
+        return self._melodies
+    
+    @property
+    def csv_chants_header(self) -> str:
+        """
+        Returns proper csv header for chants export to csv
+        """
+        return Chant.header()
+
+    @property
+    def csv_sources_header(self) -> str:
+        """
+        Returns proper csv header for sources export to csv
+        """
+        return Source.header()
+
+    def export_csv(self, chants_filepath : str, sources_filepath):
+        """ 
+        Exports the chants and sources to CSV files.
+        If sources_filepath is not provided, only chants will be exported.
+        """
+        # Chants
+        try:
+            with open(chants_filepath, 'w') as s_file:
+                print(self.csv_chants_header, file=s_file)
+                for chant in self._chants:
+                    print(chant.to_csv_row, file=s_file)
+        except Exception as e:
+            print(f"Error exporting chants file : {e}")
+
+        # Sources
+        if self._sources:
+            try:
+                with open(sources_filepath, 'w') as s_file:
+                    print(self.csv_sources_header, file=s_file)
+                    for source in self._sources:
+                        print(source.to_csv_row, file=s_file)
+            except Exception as e:
+                print(f"Error exporting sources file : {e}")
+
+
+    def drop_duplicate_chants(self):
+        """
+        Discards all chants that have the same chantlink as another chant.
+        Keeps the last occurrence of each chant.
+        """
+        chantlinks = [ch.chantlink for ch in self._chants]
+        i = 0
+        for chant in self._chants:
+            if chantlinks.count(chantlinks[i]) > 1:
+                self._chants.remove(chant)
+                chantlinks.remove(chantlinks[i])
+            i += 1
+
+    def drop_duplicate_sources(self):
+        """
+        Discards all sources that have the same srclink as another source.
+        Keeps the last occurrence of each source.
+        """
+        srclinks = [s.srclink for s in self._sources]
+        i = 0
+        for source in self._sources:
+            if srclinks.count(srclinks[i]) > 1:
+                self._sources.remove(source)
+                srclinks.remove(srclinks[i])
+            i += 1
+
+
+    def merge_with(self, to_be_merged, keep_duplicates=True):
+        """
+        Merges the current corpus with another corpus.
+        If keep_duplicates is True, it keeps all chants, sources and possibly melodies from both corpora.
+        """
+        pass
+    
+    def create_melodies(self):
+        """
+        Creates Melody objects for all chants in the corpus that has melody encoded
+        and stores them in _melodies list - locks if corpus is not editable.
+        This method should be called after the chants are loaded.
+        """
+        self._melodies = []
+        for chant in self._chants:
+            chant.create_melody()
+            self._melodies.append(chant.melody_object)
+        if not self.is_editable:
+            self._lock_melodies()
+
+    def keep_melodic_chants(self):
+        """
+        Keeps only chants that have a melody in the corpus.
+        """
+        self._chants = [ch for ch in self._chants if ch._has_melody]
+        # Just in case, we also need to create melodies again
+        self.create_melodies()
+    
+    def discard_empty_sources(self):
+        """
+        Discards all sources that have no chants in corpus.
+        """
+        sources_in_chant_data = {ch.srclink for ch in self._chants}
+        self._sources = [s for s in self._sources if s.srclink in sources_in_chant_data]
+
+
+    def apply_filter(self, filter : Filter):
+        """
+        Applies the given filter on its data in "in place" way.
+        """
+        self._chants, self._sources, self._melodies = filter.apply(self._chants, self._sources, self._melodies)
+        #print('Discarding empty sources after filtration...')
+        #self.discard_empty_sources()
+        #print('Number of chants after filtration:', len(self._chants), '\nNumber of sources after filtration:', len(self._sources))
+        
+
